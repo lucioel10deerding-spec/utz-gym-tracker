d64ceb499a8e0048a8a6fe24d73025a2317fbc6b
Lucio <deinemail@example.com>
2026-04-23 17:50:24 +0200

    Add gym storage persistence to services

--- gym_tracker/services/services.py
+++ gym_tracker/services/services.py
@@ -1,16 +1,19 @@
 from ..domain.model import GymCapacity
+from ..adapters.storage import load_gyms, save_gyms
 
 
-gyms = {}
+gyms = load_gyms()
 
 def create_gym(name : str , max_capacity:int):
     gym = GymCapacity(name,max_capacity)
     gyms[name] = gym
+    save_gyms(gyms)
     
 def enter_gym(name:str):
     gym = gyms.get(name)
     if gym:
         gym.enter()
+        save_gyms(gyms)
 
 def leave_gym(name):
     gym = gyms.get(name)
@@ -19,6 +22,7 @@ def leave_gym(name):
         return None
 
     gym.leave()
+    save_gyms(gyms)
     return {"current": gym.current_count, "max": gym.max_capacity}
 
 def get_capacity(name: str):
