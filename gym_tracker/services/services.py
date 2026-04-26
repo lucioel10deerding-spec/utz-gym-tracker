72c5391a0b5feb8670a137de115ab52f597cbdf1
Lucio <deinemail@example.com>
2026-04-26 16:01:04 +0200

    Update services logic

--- gym_tracker/services/services.py
+++ gym_tracker/services/services.py
@@ -4,18 +4,22 @@ from ..adapters.storage import load_gyms, save_gyms
 
 gyms = load_gyms()
 
-def create_gym(name : str , max_capacity:int):
-    gym = GymCapacity(name,max_capacity)
+
+def create_gym(name: str, max_capacity: int):
+    gym = GymCapacity(name, max_capacity)
     gyms[name] = gym
     save_gyms(gyms)
-    
-def enter_gym(name:str):
+
+
+def enter_gym(name: str):
     gym = gyms.get(name)
+
     if gym:
         gym.enter()
         save_gyms(gyms)
 
-def leave_gym(name):
+
+def leave_gym(name: str):
     gym = gyms.get(name)
 
     if gym is None:
@@ -23,11 +27,21 @@ def leave_gym(name):
 
     gym.leave()
     save_gyms(gyms)
-    return {"current": gym.current_count, "max": gym.max_capacity}
+
+    return {
+        "current": gym.current_count,
+        "max": gym.max_capacity,
+    }
+
 
 def get_capacity(name: str):
     gym = gyms.get(name)
+
     if gym:
-        return {"current": gym.current_count, "max": gym.max_capacity}
+        return {
+            "current": gym.current_count,
+            "max": gym.max_capacity,
+        }
+
     return None
     
